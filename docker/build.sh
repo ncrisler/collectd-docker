4a7689216226ddf0a22160caf58d436c62e95c88
ncrisler <nathan.crisler@gmail.com>
2017-10-03 14:41:29 -0400

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -24,17 +24,17 @@ export PATH="${GOPATH}/bin:/usr/local/go/bin:${PATH}"
 go get github.com/docker-infra/reefer
 go get github.com/tools/godep
 
-cd /go/src/github.com/bobrik/collectd-docker/collector
+cd /go/src/github.com/ncrisler/collectd-docker/collector
 godep restore
-go get github.com/bobrik/collectd-docker/collector/...
+go get github.com/ncrisler/collectd-docker/collector/...
 
 cd /
 
 cp /go/bin/collectd-docker-collector /usr/bin/collectd-docker-collector
 cp /go/bin/reefer /usr/bin/reefer
 
-cp /go/src/github.com/bobrik/collectd-docker/docker/collectd.conf.tpl /etc/collectd/collectd.conf.tpl
-cp /go/src/github.com/bobrik/collectd-docker/docker/run.sh /run.sh
+cp /go/src/github.com/ncrisler/collectd-docker/docker/collectd.conf.tpl /etc/collectd/collectd.conf.tpl
+cp /go/src/github.com/ncrisler/collectd-docker/docker/run.sh /run.sh
 
 apt-get remove -y git curl ca-certificates
 apt-get autoremove -y
